ae212a9d42f2a968368aa5037306bbcd97b08226
ravi-kolla <ravitejakolla29@gmail.com>
2020-11-26 22:12:59 +0530

    social share minor style fixes

--- components/socialShare.js
+++ components/socialShare.js
@@ -6,8 +6,8 @@ const SocialShare = (props) => (
     <div>
       <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-text={props.dataText} data-via="itsHDGroup" data-related="hindustandaily" data-lang="en" data-show-count="true"></a>
     </div>
-    <div className="pl-md-1">
-      <iframe src={`https://www.facebook.com/plugins/like.php?href=https://hindustandaily.com/${props.category}/${props.dataUid}&width=450&layout=standard&action=like&size=small&share=true&height=35&appId=217894516383173`} width="450" height="35" style={{border:'none',overflow:'hidden'}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+    <div className="pl-lg-2">
+      <iframe src={`https://www.facebook.com/plugins/like.php?href=https://hindustandaily.com/${props.category}/${props.dataUid}&width=450&layout=standard&action=like&size=small&share=true&height=35&appId=217894516383173`} width="300" height="35" style={{border:'none',overflow:'hidden'}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
     </div>
   </div>
 )
